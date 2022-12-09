d9f0e21def865c2667d583f5f17b722213f24851
Kashish <kashish.biserwal@protonmail.com>
2022-12-09 12:10:07 +0530

    /

--- server.js
+++ server.js
@@ -26,7 +26,9 @@ app.use("/api/auth", authRoute)
 app.use("/api/products", productRoute)
 app.use("/api/carts", cartRoute)
 
-
+app.get("/", (req, res) => {
+  res.status(200).send("Express api on vercel.")
+})
 // app.get("/products/:cat", (req, res) => {
 //   const {cat} = req.params
 //   const cat_products = products.filter((item) => {
