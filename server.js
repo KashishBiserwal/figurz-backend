a334ac7e8724ab8cd385547746b98e5853a192e4
Kashish <kashish.biserwal@protonmail.com>
2022-12-09 12:07:32 +0530

    remove error

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose")
 const dotenv = require('dotenv')
-const {products} = require("./data")
+// const {products} = require("./data")
 const userRoute = require("./routes/user")
 const authRoute = require("./routes/auth")
 const productRoute = require("./routes/product")
@@ -26,29 +26,26 @@ app.use("/api/auth", authRoute)
 app.use("/api/products", productRoute)
 app.use("/api/carts", cartRoute)
 
-app.get("/products", (req, res) => {
-  return res.json(products)
-})
 
-app.get("/products/:cat", (req, res) => {
-  const {cat} = req.params
-  const cat_products = products.filter((item) => {
-    return item.universe === cat || item.gender === cat
-  })    
-  if(!cat_products) {
-      return res.status(404).send('Product does not exist.')
-  }
-  return res.status(200).json(cat_products)
-})
+// app.get("/products/:cat", (req, res) => {
+//   const {cat} = req.params
+//   const cat_products = products.filter((item) => {
+//     return item.universe === cat || item.gender === cat
+//   })    
+//   if(!cat_products) {
+//       return res.status(404).send('Product does not exist.')
+//   }
+//   return res.status(200).json(cat_products)
+// })
 
-app.get("/singleProduct/:productId", (req, res) => {
-  const {productId} = req.params;
-  const singleProduct = products.find((product) => product.id === productId)
-  if(!singleProduct) {
-      return res.status(404).send('Product does not exist.')
-  }
-  return res.json(singleProduct)
-})
+// app.get("/singleProduct/:productId", (req, res) => {
+//   const {productId} = req.params;
+//   const singleProduct = products.find((product) => product.id === productId)
+//   if(!singleProduct) {
+//       return res.status(404).send('Product does not exist.')
+//   }
+//   return res.json(singleProduct)
+// })
 
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
